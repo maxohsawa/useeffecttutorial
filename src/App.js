adaaee56a0cb9957a18548c8775c4b9625390de5
maxohsawa <max.ohsawa@gmail.com>
2021-09-21 09:35:30 -0400

    complete useeffect tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [resourceType, setResourceType] = useState('posts');
+  const [content, setContent] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  }
+
+  useEffect( () => {
+    console.log('on mount');
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log('resource type changed');
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json()
+      .then(json => setContent(json))
+      );
+    
+  }, [resourceType]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>{windowWidth}</h1>
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setResourceType('comments')}>Comments</button>
+      <h1>{resourceType}</h1>
+      <div>
+        {content.map(item => {
+          return <pre>{JSON.stringify(item)}</pre>
+        })}
+      </div>
     </div>
-  );
+  ); 
 }
 
 export default App;
